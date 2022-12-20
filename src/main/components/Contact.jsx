00f9f6305d1590254efe9afefd17c702b121c2b8
Juan Espinosa <arq.jorrin@gmail.com>
2022-12-20 23:58:52 +1100

    Add 3 elements to Contact component

--- src/main/components/Contact.jsx
+++ src/main/components/Contact.jsx
@@ -3,11 +3,27 @@ import '../styles/Contact.css';
 const Contact = () => {
   return (
     <section id='contact'>
-      <article className='projects-headers'>
         <h2>Contact</h2>
-      </article>
-      <article className='projects-windows'>
-        
+      <article className='contact-container'>
+        <div>
+          <i className='fa-solid fa-square-phone'></i>
+          <p>0481-990-856</p>
+        </div>
+        <div>
+          <i className='fa-solid fa-envelope'></i>
+          <p>arq.jorrin@gmail.com</p>
+        </div>
+        <div>
+        <i className='fa-brands fa-linkedin'></i>
+        <p>
+          <a 
+          href='https://www.linkedin.com/in/juanespinosajorrin/'
+          target='_blank'
+          rel='noreferrer'
+          title='Go to LinkedIn'
+          >linkedin.com/in/juanespinosajorrin/</a>
+        </p>
+        </div>
       </article>
     </section>
   );
