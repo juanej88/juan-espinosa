d4591094a3cfd5841d3b2cff6f19061e4836d185
Juan Espinosa <arq.jorrin@gmail.com>
2023-01-12 23:15:20 +1100

    Make the Footer responsive and style the Contact links

--- src/main/components/Contact.jsx
+++ src/main/components/Contact.jsx
@@ -7,18 +7,12 @@ const Contact = () => {
       <article className='contact-container'>
         <a 
           className='contant-link'
-          href='tel:0481990856'
-        >
-          <i className='fa-solid fa-square-phone'></i>
-          <p>0481-990-856</p>
-        </a>
-        <a 
-          className='contant-link'
           href='mailto:arq.jorrin@gmail.com'
         >
           <i className='fa-solid fa-envelope'></i>
           <p>arq.jorrin@gmail.com</p>
         </a>
+
         <a
           className='contant-link'
           href='https://www.linkedin.com/in/juanespinosajorrin/'
@@ -28,6 +22,14 @@ const Contact = () => {
         >
           <i className='fa-brands fa-linkedin'></i>
           <p>LinkedIn</p>
+        </a>
+
+        <a 
+          className='contant-link'
+          href='tel:0481990856'
+        >
+          <i className='fa-solid fa-square-phone'></i>
+          <p>0481-990-856</p>
         </a>
       </article>
     </section>
