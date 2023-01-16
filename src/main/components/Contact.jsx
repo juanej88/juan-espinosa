df3a55f7984486ce0fa98f54a0e1bc5a81ab27a9
Juan Espinosa <arq.jorrin@gmail.com>
2023-01-16 20:15:50 +1100

    Add darkMode to Contact and change shape of links

--- src/main/components/Contact.jsx
+++ src/main/components/Contact.jsx
@@ -4,33 +4,35 @@ const Contact = () => {
   return (
     <section id='contact'>
         <h2>Contact</h2>
-      <article className='contact-container'>
-        <a 
-          className='contant-link'
-          href='mailto:arq.jorrin@gmail.com'
-        >
-          <i className='fa-solid fa-envelope'></i>
-          <p>arq.jorrin@gmail.com</p>
-        </a>
+      <article className='contact-elements'>
+        <aside className='contact-container'>
+          <a 
+            className='contant-link'
+            href='mailto:arq.jorrin@gmail.com'
+          >
+            <i className='fa-solid fa-envelope'></i>
+            <p>Email</p>
+          </a>
 
-        <a
-          className='contant-link'
-          href='https://www.linkedin.com/in/juanespinosajorrin/'
-          target='_blank'
-          rel='noreferrer'
-          title='Go to LinkedIn'
-        >
-          <i className='fa-brands fa-linkedin'></i>
-          <p>LinkedIn</p>
-        </a>
+          <a
+            className='contant-link'
+            href='https://www.linkedin.com/in/juanespinosajorrin/'
+            target='_blank'
+            rel='noreferrer'
+            title='Go to LinkedIn'
+          >
+            <i className='fa-brands fa-linkedin'></i>
+            <p>LinkedIn</p>
+          </a>
 
-        <a 
-          className='contant-link'
-          href='tel:0481990856'
-        >
-          <i className='fa-solid fa-square-phone'></i>
-          <p>0481-990-856</p>
-        </a>
+          <a 
+            className='contant-link'
+            href='tel:0481990856'
+          >
+            <i className='fa-solid fa-square-phone'></i>
+            <p>Phone</p>
+          </a>
+        </aside>
       </article>
     </section>
   );
