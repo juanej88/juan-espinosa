96c8adfa87997cbef9d114b04e1ea1207a98be68
Juan Espinosa <arq.jorrin@gmail.com>
2024-06-12 22:23:10 +0800

    Update content in About section

--- src/main/components/About.jsx
+++ src/main/components/About.jsx
@@ -52,20 +52,18 @@ const About = () => {
       <article id='about-container' className='about-container' onScroll={changeAboutIndicator}>
         <span className='about-parts'>
           <h3>Who Am I?</h3>
-          <p>I'm a Front-End Web Developer with a Bachelor Degree in Architectural Engineering</p>
+          <p>I'm a Software Developer with a passion for learning and writing practical code solutions. I thrive on tackling challenging problems that foster my growth as a programmer. My background spans across Software Development, Project Management, and Retail Management, where I've successfully managed diverse tasks.</p>
           <h3>Coding Journey</h3>
-          <p>In 2019, I started to code by building my favourite board game "Connect Four" and since that moment, I got passionate about the process of developing web applications.</p>
-          <p>In 2020, I did a website for a local business in Sydney, and two years later, I decided to change careers. Since then, I've been focused on building strong skills to get into the web development world, while building awesome applications.</p>
-          <h3>My Current Situation</h3>
-          <p>I'm working as a supervisor at Woolworths while building projects and gaining more coding skills.</p>
+          <p>From Legos to managing construction projects, I have always loved building and problem-solving. Coding is like unlocking a new puzzle, where each line forms part of functional applications. This passion continues to drive my journey as a software developer.</p>
+          <p>I started coding as a hobby five years ago and decided to shift into a career in software development this year. This led me to move from NSW to WA to immerse myself in the tech community and work on building and maintaining software applications within a team I am proud to be part of.</p>
         </span>
         <span className='about-parts'>
-          <h3>Background</h3>
-          <p>I've always liked to build things such as Lego sets and puzzles since I was a kid. Years later, I became a construction project manager which felt like building Lego sets in human scale. I saw coding as building puzzles which pieces need to be connected together to make functional applications.</p>
-          <h3>Hobbies</h3>
-          <p>I love solving coding challenges, playing videogames and hitting some mountain bike trails.</p>
-          <h3>Extras</h3>
-          <p>Thinking about it...</p>
+          <h3>Challenge I've Encountered</h3>
+          <p>While developing the Speedy Auto Centre Pty Ltd website, the owner wanted a booking system to receive email notifications for service requests. Despite GitHub's limitations, I found a solution by hosting the website on Google Cloud, enabling to send booking confirmations directly.</p>
+          <h3>Latest Project that I Developed</h3>
+          <p>I have come across interactive CSS practice websites, but none dedicated to JavaScript built-in methods. Hence, I developed "Owl Array," a web application enabling users to practice these methods within a playful gaming environment. Featuring essential functions like push, pop, and slice, users can progress through levels with the flexibility to skip ahead or complete them sequentially.</p>
+          <h3>Current Project I'm Working at</h3>
+          <p>I frequently miss celebrating the birthdays of loved ones due to overwhelming Facebook notifications from a large friend list. To remedy this, I'm developing a full-stack app named "Cookies & Dates.” This platform empowers users to selectively store and receive notifications for important dates, ensuring they never miss a special occasion.</p>
         </span>
       </article> 
       <div className='dots'>
