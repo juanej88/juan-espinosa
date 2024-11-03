14f7aa6a237ef1b0a8fbb3efb35cc8cbbe931705
Juan Espinosa <arq.jorrin@gmail.com>
2024-11-03 20:30:38 +0800

    Update content in About component

--- src/main/components/About.jsx
+++ src/main/components/About.jsx
@@ -53,18 +53,19 @@ const About = () => {
       <article id='about-container' className='about-container' onScroll={changeAboutIndicator}>
         <div className='about-parts'>
           <h3>Who Am I?</h3>
-          <p>I'm a Software Developer with a passion for learning and writing practical code solutions. I thrive on tackling challenging problems that foster my growth as a programmer. My background spans across Software Development, Project Management, and Retail Management, where I've successfully managed diverse tasks.</p>
-          <h3>Coding Journey</h3>
-          <p>From Legos to managing construction projects, I have always loved building and problem-solving. Coding is like unlocking a new puzzle, where each line forms part of functional applications. This passion continues to drive my journey as a software developer.</p>
-          <p>I started coding as a hobby five years ago and decided to shift into a career in software development this year. This led me to move from NSW to WA to immerse myself in the tech community and work on building and maintaining software applications within a team I am proud to be part of.</p>
+          <p>I'm a full-stack developer who works with React on the front end to build responsive, user-friendly interfaces, while Django and MySQL form the backbone of my back end, ensuring scalability and reliability.</p>
+          <p>My approach to design is all about minimalism. I prioritise simplicity and avoid unnecessary features, focusing on delivering essential functionality in a clean, intuitive way.</p>
+          <h3>Career Highlights</h3>
+          <p>Developed and deployed a full-featured web app for managing personal dates and milestones. The front end is hosted on Netlify with a custom domain, while the back end runs on Railway, using Docker to set up the virtual environment for a Django API and MySQL database.</p>
+          <p>Integrated with the ChatGPT API, the app allows users 10 custom messages per month. Notifications are managed with GitHub Actions, which runs every 3 hours to deliver timely reminders across time zones, ensuring each user receives notifications between 6 am and 9 am.</p>
         </div>
         <div className='about-parts'>
-          <h3>My Latest Project</h3>
-          <p>I have come across interactive CSS practice websites, but none dedicated to JavaScript built-in methods. Hence, I developed "Owl Array," a web application enabling users to practice these methods within a playful gaming environment.</p> 
-          <p>Featuring essential functions like push, pop, and slice, users can progress through levels with the flexibility to skip ahead or complete them sequentially.</p>
-          <h3>My Current Project</h3>
-          <p>I frequently miss celebrating the birthdays of loved ones due to overwhelming Facebook notifications from a large friend list. To remedy this, I'm developing a full-stack app named "Cookies & Dates." </p> 
-          <p>This platform empowers users to selectively store and receive notifications for important dates, ensuring they never miss a special occasion.</p>
+          <h3>Coding Journey</h3>
+          <p>From building with Legos to managing construction projects, I've always had a passion for creating and problem-solving. Although I initially explored robotics and mechatronics before studying architectural engineering, I now realise that my true passion lies in software development.</p>
+          <p>I started coding as a hobby five years ago, and recently, before developing my last project, I chose to make it my career. This year, I moved from NSW to WA to immerse myself in the tech community, excited to bring creativity and logic together in building impactful applications every day.</p>
+          <h3>What's Next?</h3>
+          <p>When I'm not coding, I'm exploring ways to enhance my skills and dive deeper into the tech world. My goal is to keep building innovative applications while expanding my expertise with new technologies like Next.js, TypeScript, Node.js, and Express, along with Docker and GitHub Actions for scalable deployments.</p> 
+          <p>Learning these tools is part of my journey to continuously evolve as a developer and deliver even more powerful solutions.</p>
         </div>
       </article> 
       <div className='dots'>
